6c98df413da672309152f98f1a06f2f49f28cbd0
alsgud <ys26051@naver.com>
2023-03-09 12:12:56 +0900

    초기 커밋.

---
+++ report1/report1.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int i, j, result;
+
+    for (i = 2; i <= 100; i++)
+    {
+        result = 1;
+
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                result = 0;
+                break;
+            }
+        }
+
+        if (result == 1)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
